b9c108891b8ddbab41490bfc67936e59d5ed659e
Kalyani Mahajan <mahajankalyani2005@gmail.com>
2026-05-16 19:09:34 +0530

    Add initial data for students, courses, and results

---
+++ data.sql
@@ -0,0 +1,17 @@
+INSERT INTO students VALUES
+(1, 'Alex'),
+(2, 'John'),
+(3, 'Ibrahim');
+
+INSERT INTO courses VALUES
+(101, 'DBMS'),
+(102, 'OS'),
+(103, 'DSA');
+
+INSERT INTO results (student_id, course_id, marks) VALUES
+(1, 101, 85),
+(1, 102, 78),
+(2, 101, 92),
+(2, 103, 88),
+(3, 102, 75),
+(3, 103, 80);
